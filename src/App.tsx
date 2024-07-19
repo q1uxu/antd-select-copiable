b3dc1bd2d8a339be5b708323828d793656ecfce5
秦秋旭 <qiuxu.qin@outlook.com>
2024-07-19 10:50:49 +0800

    ant select component can not select and copy text

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Select, Alert } from "antd";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Alert
+        style={{ width: 200 }}
+        type="warning"
+        showIcon
+        message="can not select and copy"
+      />
 
-export default App
+      <Select
+        disabled
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        options={[
+          { value: "jack", label: "Jack" },
+          { value: "lucy", label: "Lucy" },
+          { value: "Yiminghe", label: "yiminghe" },
+          { value: "disabled", label: "Disabled", disabled: true },
+        ]}
+      />
+    </div>
+  );
+}
